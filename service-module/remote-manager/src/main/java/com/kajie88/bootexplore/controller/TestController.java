3b388c2ec2dc73648d9e9d655d20f1992110ab1d
kajiecy <851416347@qq.com>
2018-07-09 10:31:56 +0800

    添加Util模块 为其中添加 Excel读取工具

--- service-module/remote-manager/src/main/java/com/kajie88/bootexplore/controller/TestController.java
+++ service-module/remote-manager/src/main/java/com/kajie88/bootexplore/controller/TestController.java
@@ -1,8 +1,13 @@
 package com.kajie88.bootexplore.controller;
 
+import com.kajie88.util.excel.ReadWriteExcel;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,6 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("showParam")
     public String testInter(){
+        return "测试自动部署4";
+    }
+    @RequestMapping("readExcel")
+    public String readExcel(){
+        File f = new File("D://test.xls");
+        try {
+            List<List<String>> list = ReadWriteExcel.readExcel(f);
+            System.out.println(list.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return "测试自动部署4";
     }
 }
